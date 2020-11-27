11cc00a7e2005a479b9e4fd6cc9d9dca0d823776
Samuel Jašík <55330564+sjasik@users.noreply.github.com>
2020-11-27 14:47:11 +0100

    changed "adress" to "address"

---
+++ precache-manifest.f002acb08d561b39c63c4ec7b9e84f0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "48b423621aac5f72303f",
+    "url": "/css/app.a8d2756b.css"
+  },
+  {
+    "revision": "48b423621aac5f72303f",
+    "url": "/js/app.c7eeaae8.js"
+  },
+  {
+    "revision": "813e9b9eb16b4e89dece",
+    "url": "/css/chunk-vendors.5579511a.css"
+  },
+  {
+    "revision": "813e9b9eb16b4e89dece",
+    "url": "/js/chunk-vendors.c3e4b683.js"
+  },
+  {
+    "revision": "5e9f2fda6efed953473d6a07ca69cb98",
+    "url": "/img/piknik-logo-white.5e9f2fda.svg"
+  },
+  {
+    "revision": "ad7b665b40b2a3108b37e622707fd905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
