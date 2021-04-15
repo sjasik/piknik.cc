3660fd0656ee24ff43be2de0f5499c3fd09f4eac
Samuel Jašík <55330564+sjasik@users.noreply.github.com>
2021-04-15 17:18:58 +0200

    Pricing section added

---
+++ menucka/js/slider.js
@@ -0,0 +1,63 @@
+let slider = document.getElementById("slider");
+let price = document.getElementById("price");
+let orders = document.getElementById("orders");
+
+let prices = [4, 3.5, 3, 2.5, 2];
+
+orders.innerHTML = "500";
+
+const listener = function () {
+  window.requestAnimationFrame(function () {
+    switch (slider.value) {
+      case "1":
+        price.innerHTML = prices[0].toString().replace(".", ",");
+        orders.innerHTML = "100";
+        break;
+      case "2":
+        price.innerHTML = prices[1].toString().replace(".", ",");
+        orders.innerHTML = "300";
+        break;
+      case "3":
+        price.innerHTML = prices[2].toString().replace(".", ",");
+        orders.innerHTML = "500";
+        break;
+      case "4":
+        price.innerHTML = prices[3].toString().replace(".", ",");
+        orders.innerHTML = "1000";
+        break;
+      case "5":
+        price.innerHTML = prices[4].toString().replace(".", ",");
+        orders.innerHTML = "1500+";
+    }
+  });
+};
+
+slider.addEventListener("mousedown", function () {
+  listener();
+  slider.addEventListener("mousemove", listener);
+});
+
+slider.addEventListener("mouseup", function () {
+  slider.removeEventListener("mousemove", listener);
+});
+
+slider.addEventListener("touchstart", function () {
+  listener();
+  slider.addEventListener("touchmove", listener);
+});
+
+slider.addEventListener("touchend", function () {
+  slider.removeEventListener("touchmove", listener);
+});
+
+slider.addEventListener("keydown", listener);
+
+slider.oninput = function () {
+  var value = ((this.value - this.min) / (this.max - this.min)) * 100;
+  this.style.background =
+    "linear-gradient(to right, #f18729 0%, #f14129 " +
+    value +
+    "%, #fff " +
+    value +
+    "%, #fff 100%)";
+};
